108a67f5fbb548192c3444e666fd3679edc59b37
Kshitij Gambhir <kg@nordsense.com>
2024-02-04 15:01:59 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Pump Controller package for DTU course 47332 2024
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'pump_controller',
+    author = 'K. Gambhir, L. Nyeland, R. Ziskason - DTU Energy',
+    version = '1.0',
+    description = '',
+    license = 'MPL-2.0',
+    packages = find_packages(),
+    python_requires = '>=3.10.9',
+    install_requires = [
+        'pyserial',
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'datetime',
+        'torch',
+        'scipy'
+    ]
+)
